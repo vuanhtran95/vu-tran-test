cdc5fe2d04db379cbb3880703dee21178b5b10aa
vu <vuanhtran95@gmail.com>
2024-10-08 15:08:31 +0100

    config root directory and add font awesome

---
+++ src/components/common/index.js
@@ -0,0 +1,13 @@
+import Avatar from "./Avatar";
+import Button from "./Button";
+import Card from "./Card";
+import Dropdown from "./Dropdown";
+import Footer from "./Footer";
+import Header from "./Header";
+import Icon from "./Icon";
+import Img from "./Img";
+import InputField from "./InputField";
+import Sidebar from "./Sidebar";
+import SocialMedia from "./SocialMedia";
+
+export { Avatar, Button, Card, Dropdown, Header, Footer, Icon, Img, InputField, Sidebar, SocialMedia };
